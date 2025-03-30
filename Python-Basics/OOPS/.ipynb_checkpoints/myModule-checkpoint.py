5fb7fb956f2491ac0d842e06611fa97cd7e3baf2
Guraasees Singh <guraaseessingh389@gmail.com>
2025-03-30 17:02:13 +0530

    PythonBasics

---
+++ Python-Basics/OOPS/.ipynb_checkpoints/myModule-checkpoint.py
@@ -0,0 +1,12 @@
+def moduleW():
+    print(1,"moduleeeee")
+    print(1,"moduleeeee")
+def A():
+    print("A")
+def add():
+    print(1,"mode")
+    print(1,"mode")
+
+if __name__ == '__main__':
+    print('This is main')
+# name is variable which hold name of file /module name when it is run after being imported however, when we run the file then name is main then the code of file like prints run
